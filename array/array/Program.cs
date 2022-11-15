25623b9c304c78aa45f7e66c3dfe08bba0510a55
antlerjely96 <antlerjely96@gmail.com>
2022-11-15 16:16:02 +0700

    add array

---
+++ array/array/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhap mot mang n so nguyen (n nhap tu ban phim). Tinh tong cac phan tu co gia tri chia het cho 2
+
+//Nhap n
+Console.Write("Nhap n: ");
+int n = int.Parse(Console.ReadLine());
+//Khai bao mang so nguyen co n phan tu
+int[] arr = new int[n];
+//Nhap mang
+for (int i = 0; i < n; i++)
+{
+    Console.Write("arr[" + i + "] = ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+//Tinh tong cac phan tu co gia tri chan
+int tong = 0;
+for (int i = 0; i < n; i++)
+{
+    if (arr[i] % 2 == 0)
+    {
+        tong += arr[i];
+    }
+}
+//Hien thi ket qua
+Console.WriteLine("Tong cac phan tu co gia tri chan trong mang: " + tong);
---
+++ loop/loop/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhap so nguyen n. In ra cac so tu 1 den n
+
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+/* for */
+Console.WriteLine("----- for -------");
+for (int i = 1; i < n; i++)
+{
+    Console.Write(i + "\t");
+}
+Console.WriteLine();
+/* while */
+Console.WriteLine("----- while -------");
+int j = 1;
+while (j < n)
+{
+    Console.Write(j + "\t");
+    j++;
+}
+Console.WriteLine();
+/* do - while */
+Console.WriteLine("----- do - while -------");
+int k = 1;
+do
+{
+    Console.Write(k + "\t");
+    k++;
+} while (k < n);
