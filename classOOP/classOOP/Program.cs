d263c2ea1c2b81953f8801a8ec916cc4ce5db1a6
antlerjely96 <antlerjely96@gmail.com>
2022-11-22 16:13:02 +0700

    add class vs obj

---
+++ classOOP/classOOP/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using classOOP;
+
+class Program
+{
+    public static void Main()
+    {
+        students st1 = new students();
+        st1.name = "A";
+        st1.age = 20;
+        st1.address = "HN";
+        Console.WriteLine("Student Information:");
+        Console.WriteLine("Name: " + st1.name);
+        Console.WriteLine("Age: " + st1.age);
+        Console.WriteLine("Address: " + st1.address);
+        st1.eat();
+        st1.sleep();
+        students[] student = new students[10];
+    }
+}
---
+++ classOOP/classOOP/students.cs
@@ -0,0 +1,18 @@
+﻿namespace classOOP;
+
+public class students
+{
+    public string name;
+    public int age;
+    public string address;
+
+    public void eat()
+    {
+        Console.WriteLine("eat");
+    }
+
+    public void sleep()
+    {
+        Console.WriteLine("sleep");
+    }
+}
